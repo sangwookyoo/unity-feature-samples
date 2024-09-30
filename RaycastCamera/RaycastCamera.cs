1a56018b64b95747b67f797a64e9c7e37d25c21e
sangwookyoo <tkddnr032@gmail.com>
2024-09-30 23:10:14 +0900

    feat: raycast camera

---
+++ RaycastCamera/RaycastCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RaycastCamera : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector3 mousePos = _mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _mainCamera.nearClipPlane));
+        Vector3 dir = mousePos - _mainCamera.transform.position;
+        dir = dir.normalized;
+
+        Debug.DrawRay(_mainCamera.transform.position, dir * 100, Color.red, 1f);
+
+        if (Physics.Raycast(_mainCamera.transform.position, dir, out var hit, 100f))
+        {
+            Debug.Log($"Raycast Camera @{hit.collider.gameObject.name}");
+        }
+    }
+}
